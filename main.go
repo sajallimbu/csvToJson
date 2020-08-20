83e33a046e9ae0f43794e7cb06a61bcdcd397a6f
sajallimbu <40119982+sajallimbu@users.noreply.github.com>
2020-08-20 21:55:25 +0545

    Update main.go

--- main.go
+++ main.go
@@ -18,30 +18,24 @@ type Employee struct {
 }
 
 func main() {
-	//Open your csvFile
 	csvFile, err := os.Open("./data.csv")
 	if err != nil {
 		fmt.Print(err.Error())
 	}
-	//Close the file after reading the contents
 	defer csvFile.Close()
 
-	//Initialize your csv reader
 	reader := csv.NewReader(csvFile)
 	reader.FieldsPerRecord = -1
 
-	//Read all data
 	csvData, err := reader.ReadAll()
 	if err != nil {
 		fmt.Print(err.Error())
 		os.Exit(1)
 	}
 
-	//Initialize Employee model for field interaction
 	var emp Employee
 	var employees []Employee
 
-	//Loop the data read and assign them to a vairable
 	for _, each := range csvData {
 		emp.FirstName = each[0]
 		emp.LastName = each[1]
@@ -52,7 +46,6 @@ func main() {
 		employees = append(employees, emp)
 	}
 
-	//Conver the data to JSON
 	jsonData, err := json.Marshal(employees)
 	if err != nil {
 		fmt.Print(err.Error())
@@ -61,14 +54,12 @@ func main() {
 
 	fmt.Print(string(jsonData))
 
-	//Create a new JSON file
 	jsonFile, err := os.Create("./data.json")
 	if err != nil {
 		fmt.Print(err.Error())
 	}
 	defer jsonFile.Close()
 
-	//Write the JSON data to the file
 	jsonFile.Write(jsonData)
 	jsonFile.Close()
 
